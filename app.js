e121bd6f36bded2b20576e34ae21658b4b3b859f
Jacob McClure <jacobmcclure801@gmail.com>
2020-11-18 13:45:49 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var x = 0;
+
+function addOne() {
+    x += 1;
+    document.getElementById("userNum").innerHTML = x;
+}
+
+function minusOne() {
+    x -= 1;
+    document.getElementById("userNum").innerHTML = x;
+}
